4a52b35ba2f32487002963f967d137a8f2dab442
German Oyarzo <german_oyarzo@hotmail.com>
2021-05-17 20:25:06 -0300

    probando segunda parte

--- src/OyarzoGerman/Main.java
+++ src/OyarzoGerman/Main.java
@@ -1,10 +1,12 @@
 package OyarzoGerman;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hola git");
-
+        System.out.println("Verde mundo");
     }
 }
